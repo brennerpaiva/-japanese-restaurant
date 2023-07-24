83b4381c92f95a25d8ac361341dcc5c842cc57e1
unknown <brennerpaiva@gmail.com>
2023-07-23 21:02:00 -0300

    feat: create first section

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "@/components/Header/header";
+import FirstSection from "@/components/FirstSection/firstSecton";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Header />
-      <div className={styles.description}></div>
+      <FirstSection />
     </main>
   );
 }
---
+++ src/components/FirstSection/firstSecton.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Image from "next/image";
+import Style from "./styles.module.scss";
+import ImageSection1 from "../../../public/images/section-1.png";
+
+export default function FirstSection() {
+  return (
+    <div className={Style.container}>
+      <div className={Style.text}>
+        <h1>Comida Oriental</h1>
+        <p>
+          A culinária Japonesa é bastante equilibrada, sendo muito rica em
+          peixes (ômega 3), vegetais, massas e ingredientes frescos.
+        </p>
+        <button>Cardápio</button>
+      </div>
+      <div className={Style.image}>
+        <Image src={ImageSection1} width={430} />
+      </div>
+    </div>
+  );
+}
